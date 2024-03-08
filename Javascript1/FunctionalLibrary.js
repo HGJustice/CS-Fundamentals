8073005945207fd6ccf086103b8a4020f312aae9
HGJustice <125309498+HGJustice@users.noreply.github.com>
2024-03-08 08:28:44 +0100

    Update FunctionalLibrary.js

--- Javascript1/FunctionalLibrary.js
+++ Javascript1/FunctionalLibrary.js
@@ -1,75 +1,69 @@
-function isArray(obj){
-   return obj instanceof Array
+function isArray(obj) {
+  return Array.isArray(obj);
 }
 
-function isBoolean(obj){
-    return typeof obj === "boolean"
+function isBoolean(obj) {
+  return typeof obj === "boolean";
 }
 
-function isDate(obj){
-    return obj instanceof Date
+function isDate(obj) {
+  return obj instanceof Date;
 }
 
-function isNumber(obj){
-    return typeof obj === "number"
+function isNumber(obj) {
+  return typeof obj === "number";
 }
 
-function isString(obj){
-    return typeof obj === "string"
+function isString(obj) {
+  return typeof obj === "string";
 }
 
-function isFunction(obj){
-    return obj instanceof Function
+function isFunction(obj) {
+  return obj instanceof Function;
 }
 
-function isUndefined(obj){
-    return typeof obj === "undefined"
+function isUndefined(obj) {
+  return typeof obj === "undefined";
 }
 
-function isNull(obj){
-    return obj === null
+function isNull(obj) {
+  return obj === null;
 }
 
-function first(arr){
-    return arr[0]
+function first(arr) {
+  return arr[0];
 }
 
-function last(arr){
-    return arr.at(-1)
+function last(arr) {
+  return arr.at(-1);
 }
 
-function skip(arr, numAmountToSkip){
-    for(let i = numAmountToSkip; i <= arr.length-1; i++){
-       console.log(arr[i])
-    }
+function skip(arr, numAmountToSkip) {
+  return arr.slice(numAmountToSkip, arr.length);
 }
 
-function take(arr, numAmountToTake){
-   return arr.slice(0, numAmountToTake)
+function take(arr, numAmountToTake) {
+  return arr.slice(0, numAmountToTake);
 }
 
 function asChain(arr) {
-    let chain = {
-        array: arr,
+  let chain = {
+    array: arr,
 
-        skip: function(numAmountToSkip) {
-            let newArray = [];
-            for(let i = numAmountToSkip; i < this.array.length; i++){
-                newArray.push(this.array[i]);
-            }
-            this.array = newArray; 
-            return this; 
-        },
+    skip: function (numAmountToSkip) {
+      this.array = arr.slice(numAmountToSkip, arr.length);
+      return this;
+    },
 
-        take: function(numAmountToTake) {
-            this.array = this.array.slice(0, numAmountToTake);
-            return this; 
-        },
+    take: function (numAmountToTake) {
+      this.array = this.array.slice(0, numAmountToTake);
+      return this;
+    },
 
-        value: function() {
-            return this.array;
-        }
-    };
+    value: function () {
+      return this.array;
+    },
+  };
 
-    return chain;
+  return chain;
 }
